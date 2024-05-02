8a186e44fd891d02a9048443585692726def4abc
Alireza Delkhahi <a.delkhahi@runc.info>
2024-05-02 17:09:10 +0330

    minor change

--- src/chapter_3.rs
+++ src/chapter_3.rs
@@ -1,3 +1,21 @@
 pub fn variables_practice(){
+    let a: u8;
+    let b: u8;
+    a = 250;
+    b = 10;
+    println!("a => {a}");
+    println!("b => {b}");
+    println!("a.wrapping_add(b) ====> {}", a.wrapping_add(b));
+    match a.checked_add(b){
+        Some(v) => println!("a.checked_add(b) ==> {v}"),
+        None => println!("a.checked_add(b) got None"),
+    }
+    println!("a.saturating_add(b) => {}", a.saturating_add(b));
+    match a.checked_add(b){
+        Some(v) => println!("a.checked_add(b) ==> {v}"),
+        None => println!("a.checked_add(b) got None"),
+    }
+    // println!("a + b => {:?}", c.0);
+    // println!("a + b => {:#?}", c.1);
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ pub mod chapter_3;
 
 
 fn main() {
-    chapter_1_2::guessing_number();
+    // chapter_1_2::guessing_number();
     chapter_3::variables_practice();
 }
